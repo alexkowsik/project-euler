5a9ae82833008d8829ce9df5593985565473afe1
nonamekiddie <globalanekdote@web.de>
2017-12-17 14:12:36 +0100

    Update problem005.cpp

--- problems/problem005.cpp
+++ problems/problem005.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <cmath>
-#include <string>
 
 using namespace std;
-
-
-//unsigned long --> 4294967295
-//unsigned long long --> 18446744073709551615
-
 
 int main()
 {
 	int a;
 
-	for (unsigned long i = 1000; i < 4294967000; i++) {
+	for (unsigned long i = 1000; i < 999999999; i+=20) {
 		a = 0;
 		
-		for (int j = 1; j < 21; j++) {
+		for (int j = 1; j <= 20; j++) {
 			if ((i % j) == 0)
 				a++;
+			else
+				break;
 		}
+
 		if (a == 20) {
 			cout << i << endl;
 			break;
 		}
 	}
 
-	cout << "hey" << endl;
-	cin >> a;
-
+	system("PAUSE");
 	return 0;
 }
