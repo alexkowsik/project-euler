49718090afe725bf78a2c6685c8aad2dd7b906b2
nonamekiddie <globalanekdote@web.de>
2017-12-17 21:12:50 +0100

    Update problem014.cpp

--- problems/problem014.cpp
+++ problems/problem014.cpp
@@ -3,13 +3,9 @@
 using namespace std;
 
 
-
 int main()
 {
-	unsigned long number;
-	unsigned long counter = 0;
-	unsigned long larger = 0;
-	unsigned long max = 1;
+	unsigned long number, counter, larger = 0, max = 1;
 
 	for (int i = 2; i <= 1000000; i++)
 	{
@@ -29,13 +25,11 @@ int main()
 		if (counter > larger) {
 			larger = counter;
 			max = i;
-			cout << max << endl;
 		}
 	}
 
-
-	cout << counter << endl;
-	cin >> max;
-
+	cout << max << endl;
+	
+	system("PAUSE");
 	return 0;
 }
