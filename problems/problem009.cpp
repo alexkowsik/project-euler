82328a060a1c0aa393feefb6c25bf9ca75dabf13
nonamekiddie <globalanekdote@web.de>
2017-12-17 20:59:29 +0100

    Update problem009.cpp

--- problems/problem009.cpp
+++ problems/problem009.cpp
@@ -9,14 +9,13 @@ int main()
 	int a, b, c = 3;
 	double temp = 0;
 
-
 	for (int i = 4; i < 1000000; i++) {
 		a = i;
 		b = a + 1;
 
 		for (int i = b; i < 1000000; i++) {
 			b = i;
-			temp = sqrt((a*a) + (b*b));
+			temp = sqrt((a * a) + (b * b));
 			if (temp == floor(temp)) {
 				if ((a + b + temp) == 1000)
 				break;
@@ -26,9 +25,8 @@ int main()
 				break;
 	}
 
-
-	cout << (a*b*temp) << endl;
-	cin >> a;
-
+	cout << (a * b * temp) << endl;
+	
+	system("PAUSE");
 	return 0;
 }
