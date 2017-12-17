f429e3132c88551f7eb978f42657f81b81e64b56
nonamekiddie <globalanekdote@web.de>
2017-12-17 13:38:41 +0100

    Update and rename problem001.cs to problem001.cpp

---
+++ problems/problem001.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	long sum3 = 0;
+	long sum5 = 0;
+	int a = 1;
+
+	while ((3 * a) < 1000) {
+		sum3 += (3 * a);
+		a++;
+	}
+
+	a = 1;
+
+	while ((5 * a) < 1000) {
+		if (((5 * a) % 3) != 0)
+			sum5 += (5 * a);
+		a++;
+	}
+
+	cout << sum3 + sum5 << endl;
+
+	system("PAUSE");
+	return 0;
+}
